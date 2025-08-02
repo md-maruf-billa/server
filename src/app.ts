bd11d22cbbfbb1ef73219008dfa29338a34bd18d
md-maruf-billa <eng.marufbilla@gmail.com>
2025-08-03 01:28:35 +0600

    add

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express, { Request, Response } from 'express';
+import customDomainMapper from './app/middlewares/custom_domain_mapper';
 import globalErrorHandler from './app/middlewares/global_error_handler';
 import notFound from './app/middlewares/not_found_api';
 import appRouter from './routes';
-import customDomainMapper from './app/middlewares/custom_domain_mapper';
 
 // define app
 const app = express()
@@ -22,6 +22,7 @@ app.use(cors({
     credentials: true
 }))
 app.use(express.json({ limit: "100mb" }))
+app.set('trust proxy', true);
 app.use(express.raw())
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }));
