b3f095c21f4ec6892249a9911158a93bed1b4d32
md-maruf-billa <eng.marufbilla@gmail.com>
2025-08-03 01:21:05 +0600

    add

--- src/app/modules/analytics/analytics.service.ts
+++ src/app/modules/analytics/analytics.service.ts
@@ -5,6 +5,7 @@ import { AnalyticsModel } from "./analytics.schema";
 const save_visitor_info_into_db = async (req: Request) => {
     const forwarded = req.headers['x-forwarded-for'];
     const body = req?.body;
+    console.log("From client", body)
     const ip = typeof forwarded === 'string'
         ? forwarded.split(',')[0]
         : req.socket?.remoteAddress || '';
--- src/app/utils/ipExtroctor.ts
+++ src/app/utils/ipExtroctor.ts
@@ -1,3 +1,10 @@
 export const isLocalIp = (ip: string) => {
-    return ip === '::1' || ip === '127.0.0.1' || ip.startsWith('192.168.') || ip.startsWith('10.') || ip.startsWith('172.');
-}
+    return (
+        ip === '::1' ||
+        ip === '127.0.0.1' ||
+        ip === '::ffff:127.0.0.1' || // IPv6 mapped localhost
+        ip.startsWith('192.168.') ||
+        ip.startsWith('10.') ||
+        ip.startsWith('172.')
+    );
+};
